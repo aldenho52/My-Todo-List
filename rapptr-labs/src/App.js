da1e25a6ac389192f89f5e110c7a8974bc8669de
Alden Ho <aldenho@Aldens-Mac-mini.local>
2021-03-09 11:59:41 -0700

    re added app css

--- rapptr-labs/src/App.js
+++ rapptr-labs/src/App.js
@@ -1,10 +1,11 @@
 import {Route, Switch} from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import TodoList from './components/TodoList';
+import './App.css';
 
 function App() {
   return (
-    <div>
+    <div className="App">
     <Switch>
       <Route path="/" exact component={LoginPage} />
       <Route path="/todolist" component={TodoList} />
